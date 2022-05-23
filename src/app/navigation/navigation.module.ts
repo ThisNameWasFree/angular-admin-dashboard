8cc72ed3ddb437d4b5e159c6a03d7c8535fd93e2
vitalyh <vitalyh@inuits.eu>
2022-05-23 23:16:50 +0300

    skeleton

---
+++ src/app/navigation/navigation.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { SidebarComponent } from './sidebar/sidebar.component';
+import { RouterModule } from "@angular/router";
+import { MatIconModule } from "@angular/material/icon";
+import {MatTooltipModule} from "@angular/material/tooltip";
+
+@NgModule({
+  declarations: [
+    HeaderComponent,
+    FooterComponent,
+    SidebarComponent
+  ],
+  exports: [
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule,
+    MatIconModule,
+    MatTooltipModule
+  ]
+})
+export class NavigationModule { }
