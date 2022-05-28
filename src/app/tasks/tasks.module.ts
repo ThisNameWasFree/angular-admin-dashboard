807092bde8ffb9f89410d993901952d021efa466
vitalyh <vitalyh@inuits.eu>
2022-05-28 12:08:40 +0300

    layout updates

--- src/app/tasks/tasks.module.ts
+++ src/app/tasks/tasks.module.ts
@@ -1,13 +1,15 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { TasksComponent } from './tasks/tasks.component';
+import { MatButtonModule } from "@angular/material/button";
 
 @NgModule({
   declarations: [
     TasksComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatButtonModule
   ]
 })
 export class TasksModule { }
